0e7625382ae4e3f3869554f716d6aab265a303ab
begarrett90@gmail.com <begarrett90@gmail.com>
2022-08-04 10:38:45 -0400

    updated mongo pathways and scraping

--- app.py
+++ app.py
@@ -1,36 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
 
-# In[3]:
 
-
-# from flask import Flask, render_template, redirect, url_for
-# from flask_pymongo import PyMongo
-# import scraping
 app = Flask(__name__)
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 
-# In[4]:
-
 
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
-
-
-# In[15]:
 
 
 @app.route("/scrape")
@@ -40,17 +25,6 @@ def scrape():
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 #    mars.update_one({}, {"$set":mars_data}, upsert=True)
-
-
-# In[9]:
-
-
-# mars = mongo.db.mars
-# mars.update_one({}, {"$set":mars_data}, upsert=True)
-# return redirect('/', code=302)
-
-
-# In[ ]:
 
 
 if __name__ == "__main__":
